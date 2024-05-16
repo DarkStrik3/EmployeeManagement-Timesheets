bc6e9353f75f6ced4cec806d91ea8bfc42e707e9
s1064740@haileybury.vic.edu.au <s1064740@haileybury.vic.edu.au>
2024-05-16 05:31:30 +0000

    Created frmManagerDashboard, frmLogin, ServerModule1 and frmEmployeeDashboard; Edited settings and standard-page.html

---
+++ client_code/frmEmployeeDashboard/__init__.py
@@ -0,0 +1,20 @@
+from ._anvil_designer import frmEmployeeDashboardTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class frmEmployeeDashboard(frmEmployeeDashboardTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.userID =  properties['userID']
+    # Any code you write here will run before the form opens.
+    
+    
+  def refresh(self, **event_args):
+    pass
+
+  
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,34 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+@anvil.server.callable
+def Authenticate(username, password):
+  pass
+
+@anvil.server.callable
+def getRefresh(userIdentification):
+  db = app_tables.tbltimesheets.search(UserID=userIdentification)
+  return db
+
+@anvil.server.callable
+def getUserID(name):
+  row = app_tables.tblusers.get(Username=name)
+  return row[0]
+
+@anvil.server.callable
+def getLoginExists(username, pwd):
+  return True
