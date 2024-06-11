91b7db140e7171eb572630a0bbe7d86fa950acdc
s1064740@haileybury.vic.edu.au <s1064740@haileybury.vic.edu.au>
2024-06-11 10:16:37 +0000

    Created AnalyticsReporting.RowTemplate2, TimesheetsOld.rowTemplateEmplWorkTable and TimesheetsOld; Edited frmManagerDashboard, settings, Timesheets and AnalyticsReporting

--- client_code/TimesheetsOld/__init__.py
+++ client_code/TimesheetsOld/__init__.py
@@ -1,13 +1,12 @@
-from ._anvil_designer import AnalyticsReportingTemplate
+from ._anvil_designer import TimesheetsOldTemplate
 from anvil import *
-import plotly.graph_objects as go
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class AnalyticsReporting(AnalyticsReportingTemplate):
+class TimesheetsOld(TimesheetsOldTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
--- client_code/frmManagerDashboard/__init__.py
+++ client_code/frmManagerDashboard/__init__.py
@@ -4,15 +4,53 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..EmployeeManagement import EmployeeManagement
+from ..AnalyticsReporting import AnalyticsReporting
+from ..Settings import Settings
+from ..Timesheets import Timesheets
 
 
 class frmManagerDashboard(frmManagerDashboardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.userID = properties["userID"]
+    #self.userID = properties["userID"]
     # Any code you write here will run before the form opens.
+    self.cpDashboards.add_component(Settings())
 
-  def Proceed(self, **event_args):
-    userIdentification = anvil.server.call("getUserID", self.txtUsername.text)
-    open_form("frmEmployeeDashboard", userIdentification)
+  def selectEmployeeManagement(self, **event_args):
+    self.cpDashboards.clear()
+    self.cpDashboards.add_component(EmployeeManagement())
+    self.btnEmplManage.background = "#6e6e6e"
+    self.btnAnalyticReport.background = ""
+    self.btnTimesheets.background = ""
+    self.btnSettings.background = ""
+
+  def selectAnalyticsReport(self, **event_args):
+    self.cpDashboards.clear()
+    self.cpDashboards.add_component(AnalyticsReporting())
+    self.btnEmplManage.background = ""
+    self.btnAnalyticReport.background = "#6e6e6e"
+    self.btnTimesheets.background = ""
+    self.btnSettings.background = ""
+
+  def selectTimesheets(self, **event_args):
+    self.cpDashboards.clear()
+    self.cpDashboards.add_component(Timesheets())
+    self.btnEmplManage.background = ""
+    self.btnAnalyticReport.background = ""
+    self.btnTimesheets.background = "#6e6e6e"
+    self.btnSettings.background = ""
+
+  def selectSettings(self, **event_args):
+    self.cpDashboards.clear()
+    self.cpDashboards.add_component(Settings())
+    self.btnEmplManage.background = ""
+    self.btnAnalyticReport.background = ""
+    self.btnTimesheets.background = ""
+    self.btnSettings.background = "#6e6e6e"
+  
+
+
+
+
