d91bd938b0028ff76ffecc75363b2cd0414450e0
s1064740@haileybury.vic.edu.au <s1064740@haileybury.vic.edu.au>
2024-06-18 11:16:27 +0000

    Edited AddUser

--- client_code/AddUser/__init__.py
+++ client_code/AddUser/__init__.py
@@ -16,21 +16,31 @@ class AddUser(AddUserTemplate):
 
   def addNewUser(self, **event_args):
     dateFormatCode = "%d/%m/%Y"  # The date format is specified to allow for future changes by changing only 1 variable
-    try:
-      assert Validation.validateString(self.txtFullName.text) # makes sure that the name is a valid string
-      assert Validation.validateString(self.txtTitle.text) # makes sure that the title is a valid string
-      assert Validation.validateEmail(self.txtEmail.text) # makes sure that email is valid
-      # additional rates are higher than base rate, and base rate is at least minimum wage
-      assert Validation.validateRate(float(self.txtBaseRate.text), float(self.txtExtendedRate.text), float(self.txtPubHolRate.text)) 
-      assert Validation.validateDate(self.dpDoB.date.strftime(dateFormatCode), dateFormatCode) # date is valid
-      assert str(self.ddGender.selected_value) == "Male" or "Female" or "Other" # make sure one of the specified options is selected
-      assert str(self.ddEmplType.selected_value) == "Full Time" or "Part Time" # make sure one of the specified options is selected
-      assert str(self.ddGroup.selected_value) == "Warehouse" or "Manager" or "Admin" or "Accountant" # make sure one of the specified options is selected
-      assert Validation.validatePhoneNum(self.txtPhoneNumber.text) # Makes sure that the phone number is valid
-      assert Validation.validateTFN(self.txtTFN.text) # Makes sure that TFN is valid
-      # Call the server code to pass the values and create a new user.
+    issues = ""
+    if not Validation.validateString(self.txtFullName.text): # makes sure that the name is a valid string
+      issues += "invalid name"
+    if not Validation.validateString(self.txtTitle.text): # makes sure that the title is a valid string
+      issues += "invalid title" 
+    if not Validation.validateEmail(self.txtEmail.text): # makes sure that email is valid
+      issues += "invalid email"
+    # additional rates are higher than base rate, and base rate is at least minimum wage
+    if not Validation.validateRate(float(self.txtBaseRate.text), float(self.txtExtendedRate.text), float(self.txtPubHolRate.text)):
+      issues += "invalid rates"
+    if not Validation.validateDate(self.dpDoB.date.strftime(dateFormatCode), dateFormatCode): # date is valid
+      issues += "invalid date of birth"
+    if not str(self.ddGender.selected_value) == "Male" or "Female" or "Other": # make sure one of the specified options is selected
+      issues += "invalid gender"
+    if not str(self.ddEmplType.selected_value) == "Full Time" or "Part Time": # make sure one of the specified options is selected
+      issues += "invalid employment type"
+    if not str(self.ddGroup.selected_value) == "Warehouse" or "Manager" or "Admin" or "Accountant": # make sure one of the specified options is selected
+      issues += "invalid group selected"
+    if not Validation.validatePhoneNum(self.txtPhoneNumber.text): # Makes sure that the phone number is valid
+      issues += "invalid phone number"
+    if not Validation.validateTFN(self.txtTFN.text): # Makes sure that TFN is valid
+      issues += "invalid TFN number"
+    if issues == "":
+    # Call the server code to pass the values and create a new user.
       anvil.server.call('addNewuser', self.txtFullName.text, self.txtEmail.text, self.txtTempPassword.text, self.txtPhoneNumber.text, self.dpDoB.date, self.ddGender.selected_value, self.ddEmplType.selected_value, self.ddGroup.selected_value, self.txtTitle.text, float(self.txtBaseRate.text), float(self.txtExtendedRate.text), float(self.txtPubHolRate.text), self.txtTFN.text, self.flUpload.file)
-      
       self.imgUpload.source = None
       self.flUpload.clear()
       self.txtFullName.clear()
@@ -44,8 +54,9 @@ class AddUser(AddUserTemplate):
       self.ddGender.clear()
       self.ddGroup.clear()
       self.dpDoB.clear()
-    except:
+    else:
       alert("At least one of the entries is either blank or incorrectly filled out. Please try again.")
+      alert(str(issues))
 
   def uploadProfile(self, **event_args):
     try:
