4c469f7492ca13fd25d6e2cf79b7aaee444945cb
s1064740@haileybury.vic.edu.au <s1064740@haileybury.vic.edu.au>
2024-06-23 07:50:09 +0000

    Edited frmProfile; Deleted Profile

--- client_code/frmProfile/__init__.py
+++ client_code/frmProfile/__init__.py
@@ -8,10 +8,10 @@ from anvil.tables import app_tables
 
 
 class frmProfile(frmProfileTemplate):
-  def __init__(self, **properties):
+  def __init__(self, p_parent):
     # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    self.userID = properties["userID"]
+    self.init_components()
+    self._parent = p_parent
     # Any code you write here will run before the form opens.
 
 
