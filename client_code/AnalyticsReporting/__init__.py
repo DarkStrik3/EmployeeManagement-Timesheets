e2743598dac92f389aaae6c04349a0f883cfd719
s1064740@haileybury.vic.edu.au <s1064740@haileybury.vic.edu.au>
2024-07-16 12:08:28 +0000

    Edited AnalyticsReporting

--- client_code/AnalyticsReporting/__init__.py
+++ client_code/AnalyticsReporting/__init__.py
@@ -11,23 +11,35 @@ class AnalyticsReporting(AnalyticsReportingTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.user = anvil
+    self.user = anvil.users.get_user()
     self.userID = self.user["UserID"]
     # Any code you write here will run before the form opens.
     self.allRecords = anvil.server.call('getTimesheetsManagers')
-    self.allUsers = anvil.server.call('getAllEmployees', self.)
+    self.allUsers = anvil.server.call('getAllEmployees', self.userID, "UserID", True)
 
   def refreshGraphs(self, **event_args):
     timeChoice = self.ddDates.selected_value
     typeChoice = self.ddGroup.selected_value
     infoChoice = self.ddInfoShown.selected_value
     typeArr = []
+    infoArr = []
     if typeChoice == "Groups":
       typeArr = ["Warehouse", "Management", "Admin", "Accounting"]
     elif typeChoice == "Employment Type":
       typeArr = ["Full Time", "Part Time", "Contractor"]
-    elif typeChoice == "Employees"
-    infoArr = []
-    for record in self.allRecords:
-      if 
+    elif typeChoice == "Employees":
+      for user in self.allUsers:
+        typeArr.append(user['FullName'])
+      totalAmount = 0
+      for record in self.allRecords:
+        if infoChoice == "Payout":
+          totalAmount += float(record['Payout'])
+        elif infoChoice == "Time Worked":
+          totalAmount += float(record['HoursWorked'])
+    # Set Plots
+    self.plotPieDistribution.data = go.Pie(labels=typeArr, values=infoArr)
+    self.plotGroupStats.data = 
+    
+    
+    
     
