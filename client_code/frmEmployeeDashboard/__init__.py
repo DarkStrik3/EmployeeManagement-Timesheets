1519d1c27a85be158ce162a6c3e0cdcf8cfd03f3
s1064740@haileybury.vic.edu.au <s1064740@haileybury.vic.edu.au>
2024-08-01 09:44:54 +0000

    Edited frmManagerDashboard and frmEmployeeDashboard

--- client_code/frmEmployeeDashboard/__init__.py
+++ client_code/frmEmployeeDashboard/__init__.py
@@ -14,73 +14,58 @@ from ..EditUser import EditUser
 
 class frmEmployeeDashboard(frmEmployeeDashboardTemplate):
     def __init__(self, **properties):
-        """
-        Initialize the Employee Dashboard form and set up the user profile.
-        """
-        self.init_components(**properties)
-        self.user = anvil.users.get_user()  # Get the currently logged-in user
-        self.userID = self.user['UserID']  # Store the user ID
+      """
+      Initialize the Employee Dashboard form and set up the user profile.
+      """
+      self.init_components(**properties)
+      self.user = anvil.users.get_user()  # Get the currently logged-in user
+      self.userID = self.user['UserID']  # Store the user ID
 
-        # Fetch user information from the server and set up the profile display
-        userRow = anvil.server.call('getUserInfo', self.userID)
-        self.lblProfileName.text = userRow['FullName']  # Set the profile name
-        self.imgProfile.source = userRow['Profile']  # Set the profile image
-        self.openEmployeeDashboard()  # Load the default employee dashboard component
-
-    def disableUserInput(self, **event_args):
-      self.linkHeader.remove_event_handler('click')
-      self.linkProfile.remove_event_handler('click')
-
-    def enableUserInput(self, **event_args):
-      self.linkHeader.set_event_handler('click', self.openEmployeeDashboard())
-      self.linkProfile.set_event_handler('click', self.openProfileUserDetailsClick())
-
+      # Fetch user information from the server and set up the profile display
+      userRow = anvil.server.call('getUserInfo', self.userID)
+      self.lblProfileName.text = userRow['FullName']  # Set the profile name
+      self.imgProfile.source = userRow['Profile']  # Set the profile image
+      self.openEmployeeDashboard()  # Load the default employee dashboard component
   
     def openEmployeeDashboard(self, **event_args):
-        """
-        Load the Employee Dashboard component into the container.
-        """
-        self.disableUserInput() # Make sure user cant spam
-        self.cpEmployeeDashboard.clear()  # Clear the container
-        self.spacerEmplHome.height = 222
-        self.cpEmployeeDashboard.add_component(EmployeeDashboard())  # Add the Employee Dashboard component
-        self.enableUserInput() # Lets user change screen again
+      """
+      Load the Employee Dashboard component into the container.
+      """
+      self.cpEmployeeDashboard.clear()  # Clear the container
+      self.spacerEmplHome.height = 222
+      self.cpEmployeeDashboard.add_component(EmployeeDashboard())  # Add the Employee Dashboard component
 
     def openProfileUserDetailsClick(self, **event_args):
-        """
-        Load the User Details profile component into the container.
-        """
-        self.openProfileUserDetails(self.userID) # runs the function below
+      """
+      Load the User Details profile component into the container.
+      """
+      self.openProfileUserDetails(self.userID) # runs the function below
   
     def openProfileUserDetails(self, employeeID, **event_args): # Just to catch the employee ID to get rid of an error when using the same form for both manager and employee side
-        """
-        Load the User Details profile component into the container.
-        """
-        self.disableUserInput() # Make sure user cant spam
-        self.cpEmployeeDashboard.clear()  # Clear the container
-        self.spacerEmplHome.height = 330
-        self.cpEmployeeDashboard.add_component(frmProfileUserDetails(self.userID, self))  # Add the User Details component
-        self.enableUserInput() # Lets user change screen again
+      """
+      Load the User Details profile component into the container.
+      """
+      self.cpEmployeeDashboard.clear()  # Clear the container
+      self.spacerEmplHome.height = 330
+      self.cpEmployeeDashboard.add_component(frmProfileUserDetails(self.userID, self))  # Add the User Details component
+
 
     def openProfileTimesheets(self, employeeID, **event_args):
         """
         Load the Timesheets profile component into the container.
         """
-        self.disableUserInput() # Make sure user cant spam
         self.cpEmployeeDashboard.clear()  # Clear the container
         self.spacerEmplHome.height = 0
         self.cpEmployeeDashboard.add_component(frmProfileTimesheets(self.userID, self), full_width_row=True)  # Add the Timesheets component
-        self.enableUserInput() # Lets user change screen again
 
     def openSettings(self, **event_args):
         """
         Load the Settings component into the container.
         """
-        self.disableUserInput() # Make sure user cant spam
         self.cpEmployeeDashboard.clear()  # Clear the container
         self.spacerEmplHome.height = 550
         self.cpEmployeeDashboard.add_component(Settings())  # Add the Settings component
-        self.enableUserInput() # Lets user change screen again
+
 
     def signOut(self, **event_args):
         """
@@ -93,11 +78,9 @@ class frmEmployeeDashboard(frmEmployeeDashboardTemplate):
         """
         Allow the user to edit their profile details
         """
-        self.disableUserInput() # Make sure user cant spam
         self.cpEmployeeDashboard.clear() # Clear the container
         self.spacerEmplHome.height = 310
         self.cpEmployeeDashboard.add_component(EditUser(self.userID, self, False))  # Add the Edit User component.
-        self.enableUserInput() # Lets user change screen again
 
     def menu(self, **Event_args):
         """
--- client_code/frmManagerDashboard/__init__.py
+++ client_code/frmManagerDashboard/__init__.py
@@ -17,110 +17,130 @@ from ..Functions import Other
 
 class frmManagerDashboard(frmManagerDashboardTemplate):
     def __init__(self, **properties):
-        """
-        Initialize the Manager Dashboard form with user details and default view.
-        """
-        self.init_components(**properties)  # Set up form components and data bindings
-        self.user = anvil.users.get_user()  # Get the currently logged-in user
-        self.userID = self.user['UserID']  # Store the user ID
-        self.userDetails = anvil.server.call('getUserInfo', self.userID)  # Fetch user information
-        self.lblProfileName.text = self.userDetails['FullName']  # Display user's full name
-        self.imgProfile.source = self.userDetails['Profile']  # Display user's profile picture
-        self.selectEmployeeManagement()  # Load the Employee Management view by default
+      """
+      Initialize the Manager Dashboard form with user details and default view.
+      """
+      self.init_components(**properties)  # Set up form components and data bindings
+      self.user = anvil.users.get_user()  # Get the currently logged-in user
+      self.userID = self.user['UserID']  # Store the user ID
+      self.userDetails = anvil.server.call('getUserInfo', self.userID)  # Fetch user information
+      self.lblProfileName.text = self.userDetails['FullName']  # Display user's full name
+      self.imgProfile.source = self.userDetails['Profile']  # Display user's profile picture
+      self.selectEmployeeManagement()  # Load the Employee Management view by default
 
+    def disableInput(self, **event_args):
+      self.btnAnalyticReport.enabled = False
+      self.btnEmplManage.enabled = False
+      self.btnTimesheets.enabled = False
+      self.btnSettings.enabled = False
+
+    def enableInput(self, **event_args):
+      self.btnAnalyticReport.enabled = True
+      self.btnEmplManage.enabled = True
+      self.btnTimesheets.enabled = True
+      self.btnSettings.enabled = True
+  
     def signOut(self, **event_args):
-        """
-        Log out the user and open the login form.
-        """
-        anvil.users.logout()
-        open_form('frmLogin')
+      """
+      Log out the user and open the login form.
+      """
+      anvil.users.logout()
+      open_form('frmLogin')
 
     def selectEmployeeManagement(self, **event_args):
-        """
-        Load the Employee Management component into the view.
-        """
-        self.cpDashboards.clear()
-        self.spacermanagerHome.height = 0
-        self.cpDashboards.add_component(EmployeeManagement(p_parent=self), full_width_row=True)
-        self.btnEmplManage.background = "#8f8f8f"
-        self.btnAnalyticReport.background = "#333333"
-        self.btnTimesheets.background = "#333333"
-        self.btnSettings.background = "#333333"
+      """
+      Load the Employee Management component into the view.
+      """
+      self.disableInput() # Disable button spam/abuse
+      self.cpDashboards.clear()
+      self.spacermanagerHome.height = 0
+      self.cpDashboards.add_component(EmployeeManagement(p_parent=self), full_width_row=True)
+      self.btnEmplManage.background = "#8f8f8f"
+      self.btnAnalyticReport.background = "#333333"
+      self.btnTimesheets.background = "#333333"
+      self.btnSettings.background = "#333333"
+      self.enableInput() # Re-enable button input
 
     def selectAnalyticsReport(self, **event_args):
-        """
-        Load the Analytics Reporting component into the view.
-        """
-        self.cpDashboards.clear()
-        self.spacermanagerHome.height = 0
-        self.cpDashboards.add_component(AnalyticsReporting(), full_width_row=True)
-        self.btnEmplManage.background = "#333333"
-        self.btnAnalyticReport.background = "#8f8f8f"
-        self.btnTimesheets.background = "#333333"
-        self.btnSettings.background = "#333333"
+      """
+      Load the Analytics Reporting component into the view.
+      """
+      self.disableInput() # Disable button spam/abuse
+      self.cpDashboards.clear()
+      self.spacermanagerHome.height = 0
+      self.cpDashboards.add_component(AnalyticsReporting(), full_width_row=True)
+      self.btnEmplManage.background = "#333333"
+      self.btnAnalyticReport.background = "#8f8f8f"
+      self.btnTimesheets.background = "#333333"
+      self.btnSettings.background = "#333333"
+      self.enableInput() # Re-enable button input
 
     def selectTimesheets(self, **event_args):
-        """
-        Load the Timesheets component into the view.
-        """
-        self.cpDashboards.clear()
-        self.spacermanagerHome.height = 0
-        self.cpDashboards.add_component(Timesheets(), full_width_row=True)
-        self.btnEmplManage.background = "#333333"
-        self.btnAnalyticReport.background = "#333333"
-        self.btnTimesheets.background = "#8f8f8f"
-        self.btnSettings.background = "#333333"
+      """
+      Load the Timesheets component into the view.
+      """
+      self.disableInput() # Disable button spam/abuse
+      self.cpDashboards.clear()
+      self.spacermanagerHome.height = 0
+      self.cpDashboards.add_component(Timesheets(), full_width_row=True)
+      self.btnEmplManage.background = "#333333"
+      self.btnAnalyticReport.background = "#333333"
+      self.btnTimesheets.background = "#8f8f8f"
+      self.btnSettings.background = "#333333"
+      self.enableInput() # Re-enable button input
 
     def selectSettings(self, **event_args):
-        """
-        Load the Settings component into the view.
-        """
-        self.cpDashboards.clear()
-        self.spacermanagerHome.height = 515
-        self.cpDashboards.add_component(Settings(), full_width_row=True)
-        self.btnEmplManage.background = "#333333"
-        self.btnAnalyticReport.background = "#333333"
-        self.btnTimesheets.background = "#333333"
-        self.btnSettings.background = "#8f8f8f"
+      """
+      Load the Settings component into the view.
+      """
+      self.disableInput() # Disable button spam/abuse
+      self.cpDashboards.clear()
+      self.spacermanagerHome.height = 515
+      self.cpDashboards.add_component(Settings(), full_width_row=True)
+      self.btnEmplManage.background = "#333333"
+      self.btnAnalyticReport.background = "#333333"
+      self.btnTimesheets.background = "#333333"
+      self.btnSettings.background = "#8f8f8f"
+      self.enableInput() # Re-enable button input
 
     def selectAddNewUser(self, **event_args):
-        """
-        Load the Add User component into the view.
-        """
-        self.cpDashboards.clear()
-        self.spacermanagerHome.height = 330
-        self.cpDashboards.add_component(AddUser(), full_width_row=True)
-        self.btnEmplManage.background = "#333333"
-        self.btnAnalyticReport.background = "#333333"
-        self.btnTimesheets.background = "#333333"
-        self.btnSettings.background = "#333333"
+      """
+      Load the Add User component into the view.
+      """
+      self.cpDashboards.clear()
+      self.spacermanagerHome.height = 330
+      self.cpDashboards.add_component(AddUser(), full_width_row=True)
+      self.btnEmplManage.background = "#333333"
+      self.btnAnalyticReport.background = "#333333"
+      self.btnTimesheets.background = "#333333"
+      self.btnSettings.background = "#333333"
 
     def openOwnProfileUserDetails(self, **event_args):
-        """
-        Open the current user's profile details.
-        """
-        self.openProfileUserDetails(self.userID)
+      """
+      Open the current user's profile details.
+      """
+      self.openProfileUserDetails(self.userID)
 
     def openProfileUserDetails(self, userID, **event_args):
-        """
-        Load the profile details of the specified user into the view.
-        """
-        self.cpDashboards.clear()
-        self.spacermanagerHome.height = 330
-        self.cpDashboards.add_component(frmProfileUserDetails(employeeID=userID, p_parent=self), full_width_row=True)
+      """
+      Load the profile details of the specified user into the view.
+      """
+      self.cpDashboards.clear()
+      self.spacermanagerHome.height = 330
+      self.cpDashboards.add_component(frmProfileUserDetails(employeeID=userID, p_parent=self), full_width_row=True)
 
     def openProfileTimesheets(self, userID, **event_args):
-        """
-        Load the timesheets for the specified user into the view.
-        """
-        self.cpDashboards.clear()
-        self.spacermanagerHome.height = 330
-        self.cpDashboards.add_component(frmProfileTimesheets(employeeID=userID, p_parent=self), full_width_row=True)
+      """
+      Load the timesheets for the specified user into the view.
+      """
+      self.cpDashboards.clear()
+      self.spacermanagerHome.height = 330
+      self.cpDashboards.add_component(frmProfileTimesheets(employeeID=userID, p_parent=self), full_width_row=True)
 
     def editUser(self, userID, **event_args):
-        """
-        Load the Edit User component for the specified user into the view.
-        """
-        self.cpDashboards.clear()
-        self.spacermanagerHome.height = 330
-        self.cpDashboards.add_component(EditUser(userID, self, True), full_width_row=True)
+      """
+      Load the Edit User component for the specified user into the view.
+      """
+      self.cpDashboards.clear()
+      self.spacermanagerHome.height = 330
+      self.cpDashboards.add_component(EditUser(userID, self, True), full_width_row=True)
