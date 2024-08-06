2b71fd5d6654f5389643660890642583566ec57c
s1064740@haileybury.vic.edu.au <s1064740@haileybury.vic.edu.au>
2024-08-06 01:36:33 +0000

    Edited Settings.Themes

--- client_code/Settings/Themes/__init__.py
+++ client_code/Settings/Themes/__init__.py
@@ -9,79 +9,22 @@ from anvil.tables import app_tables
 
 class Themes(ThemesTemplate):
   def __init__(self, **properties):
-    """
-    Purpose: Initialize the IndividualProfile form with default properties.
-    Input:
-        **properties (dict) - Additional properties.
-    Process: Sets form components, initializes user data, and updates UI elements with user details.
-    Output: None
-    """
     self.init_components(**properties)
-    self._parent = self.item["parent"]
-    self.user = anvil.server.call("getUser", self.item["employee"]["UserID"])
 
-    self.lblEmplName.text = self.item["employee"]["FullName"]
-    self.lblEmplID.text = self.item["employee"]["UserID"]
-    self.imgProfileImage.source = self.item["employee"]["Profile"]
-    self.lblEmplEmployment.text = self.item["employee"]["Employment"]
 
-    if not self.user["enabled"]:  # If the user is archived
-      self.btnArchiveProfile.text = "Unarchive"
-      self.lblEmplEmployment.text = "Not In Employment"
-      self.lblEmplEmployment.foreground = "#aa6041"
-      self.lblEmplID.foreground = "#aa6041"
-      self.lblEmplName.foreground = "#aa6041"
-      self.lblEmployeeIdentification.foreground = "#aa6041"
-      self.lblEmploymentStat.foreground = "#aa6041"
-      self.lblName.foreground = "#aa6041"
+    def btnColourThemePlainish_click(self, **event_args):
+        anvil.server.call('set_user_theme', 'plainish-theme')
+        get_open_form().apply_theme('cyanMode')
 
-  def editUser(self, **event_args):
-    """
-    Purpose: Trigger the editing of user details for the selected employee.
-    Input: **event_args (dict) - Additional event arguments.
-    Process: Calls the parent form's editUserDetails method.
-    Output: None
-    """
-    self._parent.editUserDetails(self.item["employee"]["UserID"])
+    def btnColourThemeDefault_click(self, **event_args):
+        anvil.server.call('set_user_theme', 'default-theme')
+        get_open_form().apply_theme('lightMode')
 
-  def openUser(self, **event_args):
-    """
-    Purpose: Open the user details profile for the selected employee.
-    Input: **event_args (dict) - Additional event arguments.
-    Process: Calls the parent form's openProfileUserDetails method.
-    Output: None
-    """
-    self._parent.openProfileUserDetails(self.item["employee"]["UserID"])
-
-  def archiveUser(self, **event_args):
-    """
-    Purpose: Archive or unarchive the user account based on its current state.
-    Input: **event_args (dict) - Additional event arguments.
-    Process:
-        - Asks for confirmation to archive/unarchive the user.
-        - Calls the server to update the archive status.
-        - Refreshes the parent form's employee list.
-    Output: None
-    """
-    if self.user["enabled"]:
-      if confirm(
-        "Are you sure you want to archive "
-        + self.item["employee"]["FullName"]
-        + "'s account?"
-      ):
-        anvil.server.call("archiveUser", self.item["employee"]["UserID"], True, None)
-        self._parent.sortFilteredEmployees()  # Refreshes the entire repeating panel
-    else:
-      if confirm(
-        "Are you sure you want to unarchive "
-        + self.item["employee"]["FullName"]
-        + "'s account?"
-      ):
-        employmentType = confirm(
-          "How are they employed?", buttons=["Full Time", "Part Time", "Contractor"]
-        )
-        if employmentType:
-          anvil.server.call(
-            "archiveUser", self.item["employee"]["UserID"], False, employmentType
-          )
-          self._parent.sortFilteredEmployees()  # Refreshes the entire repeating panel
+    def btnColourThemeDark_click(self, **event_args):
+        anvil.server.call('set_user_theme', 'dark-theme')
+        get_open_form().apply_theme('darkMode')
+      
+    def saveTheme(self, **event_args):
+      """This method is called when the button is clicked"""
+      # Command to close the form with this button
+      self.raise_event("x-close-alert", value=42)
